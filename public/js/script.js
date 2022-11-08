e07772f22ee3b4fcc2f8697bf1a5c0305690b91d
Tsaqif <mushoddaqt@gmail.com>
2022-11-08 11:44:39 +0700

    halaman undangan (integrasi front end)

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+var item = document.getElementById("main");
+
+// window.addEventListener("wheel", function (e) {
+//   if (e.deltaY > 0) item.scrollLeft += 100;
+//   else item.scrollLeft -= 100;
+// });
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/*.{html,js,css}", "./views/*.ejs", "./views/layout/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "320px",
+      },
+      fontFamily: {
+        playfair: ["Playfair Display"],
+        dancing: ["Dancing Script"],
+        poppins: ["Poppins"],
+      },
+      colors: {
+        biruTua: "#1E3135",
+      },
+      fontSize: {
+        xxs: "0.65rem",
+      },
+    },
   },
   plugins: [
     {
